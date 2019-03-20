1e851d2f54f8df88e9838f71fa0622bb0f26314b
Luke Rasmussen <luke.rasmussen@gmail.com>
2019-03-20 14:04:28 -0500

    Initial add for Python/NLP/Docker work

---
+++ 06-use_case_2/nltk-bow/bow.py
@@ -0,0 +1,3 @@
+import nltk
+from nltk import word_tokenize
+print(word_tokenize('Patient presents with shortness of breath.'))
---
+++ 06-use_case_2/test-python.py
@@ -0,0 +1,6 @@
+from platform import python_version
+
+print 'Python', python_version()
+print 'Hello, World!'
+print('Hello, World!')
+print "text", ; print 'print more text on the same line'
